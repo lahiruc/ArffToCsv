3b5c5e18aed5773f1ce837b596c4a2312a64ab60
Tikiri Diasena <tikiri1995@gmail.com>
2020-10-24 13:42:02 +0530

    add minor changes

--- Colour_Wheel/Main.java
+++ Colour_Wheel/Main.java
@@ -5,10 +5,19 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.math.*;
+import java.util.Scanner; 
+
 
 public class Main {
     public static void main(String[] args) {
         int rad = 1024;
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt(); 
+        int y = in.nextInt(); 
+        int r = 0;
+        int g = 0;
+        int bl = 0;
         BufferedImage img = new BufferedImage(rad, rad, BufferedImage.TYPE_INT_RGB);
 
         // Center Point (MIDDLE, MIDDLE)
@@ -33,9 +42,11 @@ public class Main {
 
         for (int i = 0; i < img.getWidth(); i++) {
             for (int j = 0; j < img.getHeight(); j++) {
+            	
                 int a = i - centerX;
                 int b = j - centerY;
 
+                //System.out.println(a);
                 int distance = a * a + b * b;
                 if (distance < radius) {
 //                    System.out.println("Loop in");
@@ -55,6 +66,11 @@ public class Main {
                     int blueVal = (int) (255 - ((blueDist / (float) blueRad) * 256));
 
 //                    System.out.println(redVal);
+                    if (a==x && b ==y) {
+                    	r = redVal;
+                    	g = greenVal;
+                    	bl = blueVal;
+                    }
                     Color c = new Color(redVal%256, greenVal, blueVal);
 
                     float hsbVals[] = Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(), null);
@@ -70,6 +86,9 @@ public class Main {
 
         try {
             ImageIO.write(img, "png", new File("wheel.png"));
+            System.out.println(r);
+            System.out.println(g);
+            System.out.println(bl);
         } catch (IOException e) {
             e.printStackTrace();
         }
