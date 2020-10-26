46b0db397dfb8453131bc2ea6c9f449464a947ac
Tikiri Diasena <tikiri1995@gmail.com>
2020-10-26 15:59:45 +0530

    get output to a text file

--- Colour_Wheel/Main.java
+++ Colour_Wheel/Main.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.*;
 import java.util.Scanner; 
 
@@ -15,9 +16,9 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int x = in.nextInt(); 
         int y = in.nextInt(); 
-        int r = 0;
-        int g = 0;
-        int bl = 0;
+        int rcolor = 0;
+        int gcolor = 0;
+        int bcolor = 0;
         BufferedImage img = new BufferedImage(rad, rad, BufferedImage.TYPE_INT_RGB);
 
         // Center Point (MIDDLE, MIDDLE)
@@ -67,9 +68,9 @@ public class Main {
 
 //                    System.out.println(redVal);
                     if ((a==x) && (b==-y)) {
-                    	r = redVal%256;
-                    	g = greenVal;
-                    	bl = blueVal;
+                    	rcolor = redVal%256;
+                    	gcolor = greenVal;
+                    	bcolor = blueVal;
                     	break;
                     }
                     Color c = new Color(redVal%256, greenVal, blueVal);
@@ -87,9 +88,11 @@ public class Main {
 
         try {
             ImageIO.write(img, "png", new File("wheel.png"));
-            System.out.println(r);
-            System.out.println(g);
-            System.out.println(bl);
+            PrintWriter writer = new PrintWriter("rgbcolor.txt", "UTF-8");
+            writer.println(rcolor);
+            writer.println(gcolor);
+            writer.println(bcolor);
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
